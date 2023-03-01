26dccffa6bc590e79c20b496ba951046878b3655
Filipe <filipef.paulo@gmail.com>
2023-03-01 17:35:04 -0300

    Adiciona pacote classnames para controle de das classes CSS e cria as cores necessárias ao projeto no arquivo de config do tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          text: 'hsl(200, 15%, 8%)', //Light Mode Text
+          input: 'hsl(0, 0%, 52%)', //Light Mode Input
+          background: 'hsl(0, 0%, 98%)', //Light Mode Background
+          elements: 'hsl(0, 0%, 100%)', //Light Mode Elements
+        },
+        dark: {
+          text: 'hsl(0, 0%, 100%)', //Dark Mode Text
+          background: 'hsl(207, 26%, 17%)', //Dark Mode Background
+          elements: 'hsl(209, 23%, 22%)', //Dark Mode Elements
+        },
+      },
+      fontFamily: {
+        defaultFont: ['Nunito Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
