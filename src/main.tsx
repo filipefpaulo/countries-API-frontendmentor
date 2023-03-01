6d1497ed2b0b98c70404db0b1a70025e8ac90ff0
Filipe <filipef.paulo@gmail.com>
2023-03-01 11:11:33 -0300

    Adiciona e configura o tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store';
 
+import './main.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
