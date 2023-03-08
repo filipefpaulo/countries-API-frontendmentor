963e2d6e7355e1e9dda7cb9c7af4b606a727e09a
Filipe <filipef.paulo@gmail.com>
2023-03-07 22:50:13 -0300

    Adicina o useDispatch ao hook personalizado useRedux

--- src/hooks/useRedux.ts
+++ src/hooks/useRedux.ts
@@ -1,4 +1,10 @@
-import { TypedUseSelectorHook, useSelector as useReduxSelector } from 'react-redux';
-import { RootState } from '~/store';
+import {
+  TypedUseSelectorHook,
+  useSelector as useReduxSelector,
+  useDispatch as useReduxDispatch,
+} from 'react-redux';
+import { AppDispatch, RootState } from '~/store';
 
 export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
+
+export const useDispatch = () => useReduxDispatch<AppDispatch>();
