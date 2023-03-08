b4c021cd937743936ddfce2ac080c77faa2e6f87
Filipe <filipef.paulo@gmail.com>
2023-03-07 22:48:01 -0300

    Adiciona os cards dos países a pagina inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import cx from 'classnames';
+import { useEffect } from 'react';
+import { CountrieCard } from './components/CountrieCard';
 import { Header } from './components/Header';
-import { useSelector } from './hooks/useRedux';
+import { fetchCountries } from './features/countries.slice';
+import { useDispatch, useSelector } from './hooks/useRedux';
 
 function App() {
   const darkMode = useSelector((state) => state.darkMode.mode);
+  const countries = useSelector((state) => state.countries.countries);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCountries());
+  }, [dispatch]);
 
   return (
     <div
-      className={cx('h-screen w-screen font-defaultFont', {
-        'bg-dark-background text-dark-text': darkMode,
-        'bg-light-background text-light-text': !darkMode,
+      className={cx('font-defaultFont', {
+        'text-dark-text': darkMode,
+        'text-light-text': !darkMode,
       })}
     >
+      <div
+        className={cx('fixed -z-10 h-screen w-screen', {
+          'bg-dark-background': darkMode,
+          'bg-light-background': !darkMode,
+        })}
+      />
       <Header />
+      <div className='grid grid-cols-1 gap-10 px-10 pt-40'>
+        {countries?.map((country, index) => (
+          <CountrieCard key={index} country={country} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/CountrieCard/index.tsx
@@ -0,0 +1,37 @@
+import cx from 'classnames';
+import { useSelector } from '~/hooks/useRedux';
+import { CountryInterface } from '~/interface/country.interface';
+
+interface CountrieCardProps {
+  country: CountryInterface;
+}
+
+export function CountrieCard({ country }: CountrieCardProps) {
+  const darkMode = useSelector((state) => state.darkMode.mode);
+
+  const infos = [
+    { title: 'Population', value: country.population },
+    { title: 'Region', value: country.region },
+    { title: 'Capital', value: country.capital },
+  ];
+
+  return (
+    <div
+      className={cx('w-fit rounded-md shadow-md', {
+        'bg-dark-elements': darkMode,
+        'bg-light-elements': !darkMode,
+      })}
+    >
+      <img src={country.flags.png} alt={country.flags.alt} className='rounded-t-md' />
+      <div className='p-6'>
+        <p className='pb-6 text-xl font-bold'>{country.name.common}</p>
+        {infos.map(({ title, value }, index) => (
+          <div key={index}>
+            <span className='font-semibold'>{title}: </span>
+            <span>{value}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
